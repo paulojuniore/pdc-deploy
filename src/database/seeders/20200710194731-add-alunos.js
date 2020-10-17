60728991073c5f3efb52686f2fb3a62cff5311f6
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-10-16 21:18:33 -0300

    Alterando nome dos arquivos .data e mudança no bd.

--- src/database/seeders/20200710194731-add-alunos.js
+++ src/database/seeders/20200710194731-add-alunos.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const { readFile, buildDiscentes } = require('../../database/populateDB');
-var filePath = path.resolve(__dirname, '..', '..', '..', 'data', 'NewDiscente.data');
+var filePath = path.resolve(__dirname, '..', '..', '..', 'data', 'Discente.data');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
