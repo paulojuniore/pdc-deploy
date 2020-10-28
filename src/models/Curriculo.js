e942c43671ca44458bea5eac48f74c52f5c2aa08
Paulo Júnior <paulo.junior@ccc.ufcg.edu.br>
2020-10-28 20:28:09 -0300

    Adding models and migration of the Curriculo renamed.

---
+++ src/models/Curriculo.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Curriculo extends Model {
+  static init(connection) {
+    super.init({
+      codigo_disciplina: {
+        type: DataTypes.STRING(7),
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      id_tipo_disciplina: DataTypes.INTEGER,
+      id_unidade_academica: DataTypes.INTEGER,
+    }, {
+      sequelize: connection,
+      timestamps: false,
+    })
+  }
+}
+
+module.exports = Curriculo;
